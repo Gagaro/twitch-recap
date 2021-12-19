d236c983354d6dc52cdba2d29d933e7b58166000
Gagaro <gagaro42@gmail.com>
2021-12-19 17:13:24 +0100

    Prod config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/twitch-recap/',
+  build: {
+    outDir: 'docs',
+  },
   plugins: [vue()]
 })
